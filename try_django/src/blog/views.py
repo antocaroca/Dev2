3c566ab7b62105d8c64689fefe84a87726f0f316
antocaroca <antonella.caroca@cci-entel.cl>
2019-04-18 16:07:01 -0400

    agrego bootstrap

--- try_django/src/blog/views.py
+++ try_django/src/blog/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import BlogPost
+
+# def blog_post_detail_page(request, slug):
+#     obj = get_object_or_404(BlogPost, slug=slug)
+#    # obj = BlogPost.objects.get(slug= slug)
+#     template_name = 'blog_post_detail.html'
+#     context = {"object": obj} # {"title": obj.title}
+#     return render(request, template_name, context)
+
+# CRUD
+# GET --> Retrieve / List
+# GET --> Retrieve / List
+# POST --> Create / Update / Delete
+
+# Create Retrieve Update Delete
+
+def blog_post_list_view(request):
+    # list out objects
+    # could be search
+    qs = BlogPost.objects.all() # list of python objects
+    template_name = 'blog/list.html'
+    context  = { 'object_list' : qs}
+    return render(request, template_name, context)
+
+def blog_post_create_view(request):
+    # create objects
+    # how? use a form
+    template_name = 'blog/create.html'
+    context = {'form' : None}
+    return render(request, template_name, context)
+
+def blog_post_detail_view(request, slug):
+    # retieve_view = detail_view
+    # 1 object --> detail view
+    obj = get_object_or_404(BlogPost, slug=slug)
+    template_name = 'blog/detail.html'
+    context = {'object': obj}
+    return render(request, template_name, context)
+
+def blog_post_update_view(request):
+    obj = get_object_or_404(BlogPost, slug=slug)
+    template_name = 'blog/update.html'
+    context = {'object': obj, 'form': None}
+    return render(request, template_name, context)
+
+
+def blog_post_delete_view(request):
+    obj = get_object_or_404(BlogPost, slug=slug)
+    template_name = 'blog/delete.html'
+    context = {'object': obj}
+    return render(request, template_name, context)
+
--- try_django/src/try_django/urls.py
+++ try_django/src/try_django/urls.py
@@ -13,19 +13,37 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
-from .views import(
+from django.urls import path, re_path, include # url
+from blog.views import (
+    blog_post_create_view,
+)
+
+
+from .views import (
     home,
     about,
     contact,
-    example_page,
+    example_page
 )
 
+
 urlpatterns = [
+    path('', home),
+    path('blog-new/', blog_post_create_view),
+    path('blog/', include('blog.urls')),
+    # re_path(r'^blog/(?P<slug>\w+)/$', blog_post_detail_view),
+    re_path(r'^about/$', about),
+    path('example/', example_page),
+    path('contact/', contact),
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
-    path('about/', about, name='about'),
-    path('contact/', contact, name='contact'),
-    path('example/', example_page, name='example'),
 ]
+
+
+
+
+
+
+
+
